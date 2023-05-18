ef0d7f8a8745afaf0556dbfd29aec743fea54690
Joel Vaz <joelvaz.whitehat@gmail.com>
2023-05-18 19:31:59 +0100

    Nova versão handle buttons

--- src/main/java/pt/ubi/wordle/GameController.java
+++ src/main/java/pt/ubi/wordle/GameController.java
@@ -1,16 +1,18 @@
 package pt.ubi.wordle;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.RowConstraints;
+import javafx.stage.Stage;
 
-import java.util.Random;
+import java.io.IOException;
 
 public class GameController {
+
+    @FXML private Button bntExit;
 
     int difficulty = 4;
 
@@ -21,8 +23,6 @@ public class GameController {
 
     @FXML
     void initialize () {
-
-
         for (int i = 0; i < difficulty; i++) {
             TextField textField = new TextField();
             textField.setId(String.valueOf(i));
@@ -32,11 +32,16 @@ public class GameController {
                     " -fx-alignment: center;" +
                     " -fx-pref-width: 60px;" +
                     " -fx-pref-height: 60px;");
-
             gameBox.getChildren().add(textField);
         }
     }
 
-
-
+    @FXML
+    void handleExitButton() throws IOException {
+        Stage currentStage = (Stage) bntExit.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("start-view.fxml"));
+        Scene newScene = new Scene(fxmlLoader.load());
+        currentStage.setTitle("Wordle Settings");
+        currentStage.setScene(newScene);
+    }
 }
--- src/main/java/pt/ubi/wordle/StartController.java
+++ src/main/java/pt/ubi/wordle/StartController.java
@@ -2,16 +2,11 @@ package pt.ubi.wordle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.IOException;
-import java.util.Random;
 
 public class StartController {
 
@@ -23,23 +18,23 @@ public class StartController {
     void handlePlayButton() throws IOException {
         Stage currentStage = (Stage) bntPlay.getScene().getWindow();
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("game-view.fxml"));
-        Scene newScene = new Scene(fxmlLoader.load(), currentStage.getWidth(), currentStage.getHeight());
+        Scene newScene = new Scene(fxmlLoader.load());
         currentStage.setTitle("Wordle Game");
         currentStage.setScene(newScene);
     }
 
     @FXML protected void handleSettingsButton() throws IOException {
-        Stage currentStage = (Stage) bntPlay.getScene().getWindow();
+        Stage currentStage = (Stage) bntSetting.getScene().getWindow();
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("settings-view.fxml"));
-        Scene newScene = new Scene(fxmlLoader.load(), currentStage.getWidth(), currentStage.getHeight());
+        Scene newScene = new Scene(fxmlLoader.load());
         currentStage.setTitle("Wordle Settings");
         currentStage.setScene(newScene);
     }
 
     @FXML protected void handleProfileButton() throws IOException {
-        Stage currentStage = (Stage) bntPlay.getScene().getWindow();
+        Stage currentStage = (Stage) bntProfile.getScene().getWindow();
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("profile-view.fxml"));
-        Scene newScene = new Scene(fxmlLoader.load(), currentStage.getWidth(), currentStage.getHeight());
+        Scene newScene = new Scene(fxmlLoader.load());
         currentStage.setTitle("Wordle Profile");
         currentStage.setScene(newScene);
     }
