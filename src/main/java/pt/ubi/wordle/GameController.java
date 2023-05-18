849e998e22ef0e34d533b4dda2ab7fcd5f23a198
Leonel Matos <leonellmts@gmail.com>
2023-05-18 19:13:49 +0100

    O problema não é meu
    Resolve

--- src/main/java/pt/ubi/wordle/GameController.java
+++ src/main/java/pt/ubi/wordle/GameController.java
@@ -2,32 +2,41 @@ package pt.ubi.wordle;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.RowConstraints;
+
+import java.util.Random;
 
 public class GameController {
 
     int difficulty = 4;
 
     String word = "java";
-    @FXML
-    private Label welcomeText;
-    @FXML
-    private GridPane grid;
 
     @FXML
-    protected void addGridCell() {
-        System.out.println("Check");
+    private HBox gameBox;
+
+    @FXML
+    void initialize () {
 
 
-        for (int row = 0; row < difficulty; row++) {
-            //grid.addRow(row, new Label("Row " + (row+1)));
-            grid.getColumnConstraints().add(new ColumnConstraints(50));
-        }
-        for (int col = 0; col < difficulty; col++) {
-            //grid.addColumn(col, new Label("Column " + (col+1)));
-            grid.getRowConstraints().add(new RowConstraints(50));
+        for (int i = 0; i < difficulty; i++) {
+            TextField textField = new TextField();
+            textField.setId(String.valueOf(i));
+            textField.setStyle("-fx-background-color: #ACB0F2;" +
+                    " -fx-font-family: Georgia;" +
+                    " -fx-font-size: 34px;" +
+                    " -fx-alignment: center;" +
+                    " -fx-pref-width: 60px;" +
+                    " -fx-pref-height: 60px;");
+
+            gameBox.getChildren().add(textField);
         }
     }
+
+
+
 }
