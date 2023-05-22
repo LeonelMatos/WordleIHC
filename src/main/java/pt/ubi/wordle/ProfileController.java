8dfbd092318320db540ab9230a9a2225ee94d04a
Leonel Matos <leonellmts@gmail.com>
2023-05-23 00:09:13 +0100

    criar perfis

--- src/main/java/pt/ubi/wordle/ProfileController.java
+++ src/main/java/pt/ubi/wordle/ProfileController.java
@@ -2,15 +2,21 @@ package pt.ubi.wordle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.scene.layout.Priority;
 
 import java.io.IOException;
+import java.util.Random;
 
 public class ProfileController {
 
@@ -23,24 +29,85 @@ public class ProfileController {
 
     @FXML
     void instantiateProfileBox() {
+
+        Font font = new Font("Georgia", 24);
+        Font font2 = new Font("Georgia", 14);
+
+        //Set ProfileBox
         profileCounter++;
         HBox profileBox = new HBox();
         profileBox.setAlignment(Pos.CENTER);
         profileBox.setSpacing(50);
-        profileBox.setStyle("-fx-background-color: #AAAAAA");
+        profileBox.setPrefHeight(100);
+        profileBox.setPrefWidth(200);
+        profileBox.setStyle("-fx-background-color: #BBBBFF");
         int index = profileHolder.getChildren().size();
+        profileBox.setSpacing(10);
+        profileBox.setPadding(new Insets(20));
         profileHolder.getChildren().add(index, profileBox);
 
+        //Set ColorId
 
+        Circle colorId = new Circle(30, Color.color(generateColor(), generateColor(), generateColor()));
+        profileBox.getChildren().add(colorId);
+
+        //Set VBox1
+        VBox vBox1 = new VBox();
+        vBox1.setAlignment(Pos.CENTER_LEFT);
+        HBox.setHgrow(vBox1, Priority.SOMETIMES);
+        profileBox.getChildren().add(vBox1);
+
+        //Set VBox2
+        VBox vBox2 = new VBox();
+        vBox2.setAlignment(Pos.CENTER);
+        vBox2.setSpacing(20);
+        profileBox.getChildren().add(vBox2);
+
+        //Set ProfileId
         Label title = new Label("Perfil " + profileCounter);
-        title.setStyle("-fx-background-color: #ACB0F2;" +
-                " -fx-font-family: Georgia;" +
-                " -fx-font-size: 18px;" +
+        title.setStyle(" -fx-font-family: Georgia;" +
+                " -fx-font-size: 14px;" +
                 " -fx-alignment: center;" +
-                " -fx-pref-width: 100px;" +
-                " -fx-pref-height: 50px;" +
                 " -fx-font-weight: bold");
-        profileBox.getChildren().add(title);
+        title.setFont(font2);
+        HBox.setHgrow(title, Priority.SOMETIMES);
+        vBox1.getChildren().add(title);
+
+        //Set Name
+        Label name = new Label("Leonel");
+        title.setStyle(" -fx-font-family: Georgia;" +
+                " -fx-font-size: 24px;" +
+                " -fx-alignment: center;" +
+                " -fx-font-weight: bold");
+        name.setFont(font);
+        HBox.setHgrow(name, Priority.SOMETIMES);
+        vBox1.getChildren().add(name);
+
+        //Set Edit
+        Button edit = new Button("Editar");
+        edit.setStyle(" -fx-background-color: #EEEEEE;" +
+                " -fx-font-family: Georgia;" +
+                " -fx-font-size: 14px;" +
+                " -fx-font-weight: bold");
+        vBox2.getChildren().add(edit);
+
+        //X Edit
+        Button remove = new Button("X");
+        remove.setStyle(" -fx-background-color: #FF6962;" +
+                " -fx-font-family: Georgia;" +
+                " -fx-font-size: 14px;" +
+                " -fx-font-weight: bold");
+        vBox2.getChildren().add(remove);
+    }
+
+    double generateColor() {
+        Random random = new Random();
+        double min = 0.4;  // Minimum component value
+        double max = 0.8;  // Maximum component value
+
+        double range = max - min;
+        double value = random.nextDouble() * range + min;
+        return Math.min(1.0, value);
     }
 
     @FXML
--- src/main/java/pt/ubi/wordle/StartApplication.java
+++ src/main/java/pt/ubi/wordle/StartApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class StartApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("start-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("profile-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 600);
         stage.setTitle("WordleIHC");
         stage.setScene(scene);
