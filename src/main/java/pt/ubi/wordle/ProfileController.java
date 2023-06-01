6e4c2e1ad57420ee1daedb264630c690713af759
Leonel Matos <leonellmts@gmail.com>
2023-06-01 03:11:51 +0100

    Criar/Remover perfis
    Já dá para criar/remover, FALTA EDITAR O NOME (basta repetir o código para criar, mas ao contrário).
    Também falta quando acabo de criar o perfil de adicioná-lo ao ficheiro settings.txt. Depois também tenho de selecionar o perfil atual (um setOnClick ou algo assim nas vboxes do nome e colorId, seleciona o perfil e fecha a view.
    PROBLEMA: posso abrir a view de perfis no menu inicial ou durante o jogo. Como posso fazer para voltar para a view anterior? Quando estou no meio de um jogo, clico em PERFIL, seleciono outro, volto para o jogo?
    Ok, boa noite.

--- src/main/java/pt/ubi/wordle/ProfileController.java
+++ src/main/java/pt/ubi/wordle/ProfileController.java
@@ -1,5 +1,7 @@
 package pt.ubi.wordle;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
@@ -7,11 +9,11 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.layout.Priority;
 
@@ -23,19 +25,23 @@ public class ProfileController {
     private int profileCounter = 0;
 
     @FXML
+    private Button newProfileButton;
+    @FXML
     private VBox profileHolder;
     @FXML
     private Button btnExit;
 
+    //Usado para saber qual remover quando pedido
+    private HBox currentProfileBox;
+
     @FXML
     void instantiateProfileBox() {
 
-        Font font = new Font("Georgia", 24);
-        Font font2 = new Font("Georgia", 14);
-
+        newProfileButton.setDisable(true);
         //Set ProfileBox
         profileCounter++;
         HBox profileBox = new HBox();
+        currentProfileBox = profileBox;
         profileBox.setAlignment(Pos.CENTER);
         profileBox.setSpacing(50);
         profileBox.setPrefHeight(100);
@@ -61,6 +67,7 @@ public class ProfileController {
         VBox vBox2 = new VBox();
         vBox2.setAlignment(Pos.CENTER);
         vBox2.setSpacing(20);
+        //HBox.setHgrow(vBox2, Priority.SOMETIMES);
         profileBox.getChildren().add(vBox2);
 
         //Set ProfileId
@@ -69,27 +76,33 @@ public class ProfileController {
                 " -fx-font-size: 14px;" +
                 " -fx-alignment: center;" +
                 " -fx-font-weight: bold");
-        title.setFont(font2);
-        HBox.setHgrow(title, Priority.SOMETIMES);
+        //HBox.setHgrow(title, Priority.SOMETIMES);
         vBox1.getChildren().add(title);
 
+        TextField textField = new TextField();
+        textField.setText("nome");
+        textField.setStyle(" -fx-font-family: Georgia;" +
+                " -fx-font-size: 18px");
+        vBox1.getChildren().add(textField);
+
         //Set Name
-        Label name = new Label("Leonel");
-        title.setStyle(" -fx-font-family: Georgia;" +
+        /*Label name = new Label("Leonel");
+        name.setStyle(" -fx-font-family: Georgia;" +
                 " -fx-font-size: 24px;" +
                 " -fx-alignment: center;" +
                 " -fx-font-weight: bold");
-        name.setFont(font);
-        HBox.setHgrow(name, Priority.SOMETIMES);
+        //HBox.setHgrow(name, Priority.SOMETIMES);
         vBox1.getChildren().add(name);
+        */
 
         //Set Edit
-        Button edit = new Button("Editar");
-        edit.setStyle(" -fx-background-color: #EEEEEE;" +
+        Button create = new Button("Criar");
+        create.setStyle(" -fx-background-color: #EEEEEE;" +
                 " -fx-font-family: Georgia;" +
                 " -fx-font-size: 14px;" +
                 " -fx-font-weight: bold");
-        vBox2.getChildren().add(edit);
+        create.setOnAction(submitProfilePrompt);
+        vBox2.getChildren().add(create);
 
         //X Edit
         Button remove = new Button("X");
@@ -97,8 +110,59 @@ public class ProfileController {
                 " -fx-font-family: Georgia;" +
                 " -fx-font-size: 14px;" +
                 " -fx-font-weight: bold");
+        remove.setOnAction(removeProfilePrompt);
         vBox2.getChildren().add(remove);
+
     }
+
+
+    // Actions para editar/submeter/remover
+    EventHandler<ActionEvent> removeProfile = actionEvent -> {
+        profileHolder.getChildren().remove(currentProfileBox);
+        profileCounter--;
+
+        if (profileHolder.getChildren().size() < 3)
+            newProfileButton.setDisable(false);
+    };
+
+    EventHandler<ActionEvent> submitProfilePrompt = actionEvent -> {
+
+        //Verifica se existem demasiados perfis. 4+ pode partir a view
+        if (profileHolder.getChildren().size() < 3)
+            newProfileButton.setDisable(false);
+
+
+        VBox vbox1 = (VBox) currentProfileBox.getChildren().get(1);
+        TextField nameField = (TextField) vbox1.getChildren().get(1);
+        String name = nameField.getText();
+
+        vbox1.getChildren().remove(1);
+        Label nameLabel = new Label(name);
+        nameLabel.setStyle(" -fx-font-family: Georgia;" +
+                " -fx-font-size: 24px;" +
+                " -fx-alignment: center;" +
+                " -fx-font-weight: bold");
+        vbox1.getChildren().add(nameLabel);
+
+
+        VBox vbox2 = (VBox) currentProfileBox.getChildren().get(2);
+        vbox2.getChildren().remove(0);
+        Button create = new Button("Editar");
+        create.setStyle(" -fx-background-color: #EEEEEE;" +
+                " -fx-font-family: Georgia;" +
+                " -fx-font-size: 14px;" +
+                " -fx-font-weight: bold");
+        vbox2.getChildren().add(0, create);
+
+        Button removeButton = (Button) vbox2.getChildren().get(1);
+        removeButton.setOnAction(removeProfile);
+    };
+
+    EventHandler<ActionEvent> removeProfilePrompt = actionEvent -> {
+        profileHolder.getChildren().remove(currentProfileBox);
+        profileCounter--;
+        newProfileButton.setDisable(false);
+    };
 
     double generateColor() {
         Random random = new Random();
